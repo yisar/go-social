0626c5524d1842cc27fe88ff227ca5890e5bd557
yisar <1533540012@qq.com>
2022-12-10 11:39:08 +0800

    日常提交

--- model/init.go
+++ model/init.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"time"
+	"os"
 )
 
 var Mongo = InitMongo()
@@ -13,10 +14,11 @@ var Mongo = InitMongo()
 func InitMongo() *mongo.Database {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	pwd := os.Getenv("DATABASE_PWD")
 	client, err := mongo.Connect(ctx, options.Client().SetAuth(options.Credential{
-		Username: "admin",
-		Password: "admin",
-	}).ApplyURI("mongodb://localhost:27017"))
+		Username: "mongo",
+		Password: pwd,
+	}).ApplyURI("mongodb://45.207.47.90:27017"))
 	if err != nil {
 		log.Println("Connection MongoDB Error:", err)
 		return nil
