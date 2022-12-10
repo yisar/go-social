49282e55df95de0fcbe1b607808a93b945b01dc6
yisar <1533540012@qq.com>
2022-12-10 14:54:48 +0800

    前端开工

--- fre/src/login/login.tsx
+++ fre/src/login/login.tsx
@@ -28,7 +28,7 @@ export default function Login() {
         })
     }
     return <div class="login">
-        <li><h1>CliCli.登录</h1></li>
+        <li><h1>海棠。</h1><h2>文学城</h2></li>
         <li><input type="text" placeholder="昵称" onInput={(e) => changeName(e.target.value)} /></li>
         <li><input type="text" placeholder="密码" onInput={(e) => changePwd(e.target.value)} /></li>
         <li><button onClick={login}>登录</button></li>
