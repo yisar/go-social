08af02d20ecb14e63db8e9a012e3c91cb31d1fc4
yisar <1533540012@qq.com>
2023-01-26 18:57:01 +0800

    更换环境变量

--- model/init.go
+++ model/init.go
@@ -15,7 +15,7 @@ func InitMongo() *mongo.Database {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	str:=os.Getenv("DATABASE_STR")
+	str:=os.Getenv("MONGO_STR")
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(str))
 	if err != nil {
 		log.Println("Connection MongoDB Error:", err)
