d6a38a73ec769343a93f7db4a9960bc91c25a6b0
yisar <1533540012@qq.com>
2023-01-27 00:01:14 +0800

    日常提交

--- fre/src/util/avatar.ts
+++ fre/src/util/avatar.ts
@@ -2,5 +2,5 @@ import md5 from 'blueimp-md5'
 
 export function getAvatar(avatar) {
     let hash = md5(avatar)
-    return `https://gravatar.loli.net/avatar/${hash}?s=100&d=retro`
+    return `https://sdn.geekzu.org/avatar/${hash}?s=100&d=retro`
 }
